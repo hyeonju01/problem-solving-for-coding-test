6017bd95f4098e3c96a1327d43df06607c084f75
hyeonju01 <tangled6648@gmail.com>
2022-12-15 01:54:59 +0900

    feat: [CodeUp] 1013 완료

---
+++ src/Dec_2022/day_15th/CodeUp_1013.java
@@ -0,0 +1,12 @@
+package Dec_2022.day_15th;
+
+import java.util.Scanner;
+
+public class CodeUp_1013 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.printf("%d %d", a, b);
+    }
+}
