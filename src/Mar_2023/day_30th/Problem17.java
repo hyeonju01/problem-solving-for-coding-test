cb0301b3417e33ea158921ca7c791d56961b89e3
hyeonju01 <tangled6648@gmail.com>
2023-03-30 14:39:57 +0900

    feat: [Programmers] 배열의 평균값

---
+++ src/Mar_2023/day_30th/Problem17.java
@@ -0,0 +1,20 @@
+package Mar_2023.day_30th;
+
+/**
+ * [프로그래머스] 배열의 평균값
+ * 배열 모든 원소를 순회하며 합하고, 그 합을 배열 원소 개수로 나눈다. 자료형에 주의.
+ */
+
+public class Problem17 {
+    class Solution {
+        public double solution(int[] numbers) {
+            double answer = 0;
+            double sum = 0;
+            for (int i = 0; i < numbers.length; i++) {
+                sum = sum + numbers[i];
+            }
+            answer = sum / numbers.length;
+            return answer;
+        }
+    }
+}
