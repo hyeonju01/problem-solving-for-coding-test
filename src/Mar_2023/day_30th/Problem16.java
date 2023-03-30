1318725e57a58d5aa9a0d6fed64b3a8eb45ff1cd
hyeonju01 <tangled6648@gmail.com>
2023-03-30 13:37:09 +0900

    feat: [Programmers] 짝수의 합

---
+++ src/Mar_2023/day_30th/Problem16.java
@@ -0,0 +1,21 @@
+package Mar_2023.day_30th;
+
+/**
+ *
+ *  [프로그래머스] 짝수의 합
+ *  정수 n 이하의 모든 짝수를 합하여 반환하기
+ */
+
+public class Problem16 {
+    class Solution {
+        public int solution(int n) {
+            int answer = 0;
+            for (int i = 1; i < n+1; i++) {
+                if (i % 2 == 0) {
+                    answer = answer + i;
+                }
+            }
+            return answer;
+        }
+    }
+}
