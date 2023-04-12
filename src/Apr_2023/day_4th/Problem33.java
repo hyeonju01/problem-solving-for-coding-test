1f03c92f04f5f4bbe5e1b117a91a049d634fa594
hyeonju01 <tangled6648@gmail.com>
2023-04-12 16:33:54 +0900

    [Programmers] 편지

---
+++ src/Apr_2023/day_4th/Problem33.java
@@ -0,0 +1,13 @@
+package Apr_2023.day_4th;
+
+/**
+ * [프로그래머스] 편지
+ *  편지지 최소 가로길이는 머쓱이가 쓴 메세지의 length()의 2배이다.
+ */
+public class Problem33 {
+    class Solution {
+        public int solution(String message) {
+            return message.length() * 2;
+        }
+    }
+}
