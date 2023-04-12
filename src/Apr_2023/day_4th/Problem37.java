27d9b17c76051acbda04cb7f641a6dd59ce1fb4c
hyeonju01 <tangled6648@gmail.com>
2023-04-12 16:34:42 +0900

    [Programmers] 개미 군단

---
+++ src/Apr_2023/day_4th/Problem37.java
@@ -0,0 +1,31 @@
+package Apr_2023.day_4th;
+
+/**
+ *  개미 군단
+ */
+public class Problem37 {
+
+    class Solution {
+        public int solution(int hp) {
+            int answer = 0; // 개미 병력 수
+
+            final int general = 5;
+            final int soldier = 3;
+            final int worker = 1;
+
+            int general_num = hp / general;
+            answer = answer + general_num;
+            hp = hp % general;
+
+            int soldier_num = hp / soldier;
+            answer = answer + soldier_num;
+            hp = hp % soldier;
+
+            int worker_num = hp / worker;
+            answer = answer + worker_num;
+
+            return answer;
+        }
+    }
+    
+}
